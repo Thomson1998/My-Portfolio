19e33257cb236c3d2352253cc3ad908efa55c56b
Thomson1998 <jackzthomson2@gmail.com>
2023-05-31 17:37:16 +0530

    first commit

--- src/Components/Icons.jsx
+++ src/Components/Icons.jsx
@@ -62,7 +62,7 @@ const Icons = () => {
 
                 {links.map(({id, child, href, style, download}) =>(
                      <li key={id} className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 + ' ' + ${style}`}>
-                     <a href={href} className='flex justify-between items-center w-full text-white' download={download} target='_blank' rel='noopener noreferrer'>
+                     <a href={href} className='flex justify-between items-center w-full text-white' target='_blank' rel='noopener noreferrer'>
                        {child}
                      </a>
                      </li>
